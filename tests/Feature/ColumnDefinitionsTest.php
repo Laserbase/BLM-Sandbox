c6385a79c0ad01c61fc3e71ac3c351491eeb920a
John Griffiths <johng@Johns-MacBook-Pro.local>
2019-12-08 10:53:27 +0000

    Add body to test test_allColumnDefinitionsAreInspected()

--- tests/Feature/ColumnDefinitionsTest.php
+++ tests/Feature/ColumnDefinitionsTest.php
@@ -10,19 +10,66 @@ use Src\BlmFile\BlmFile;
 class ColumnDefinitionsTest extends TestCase
 {
     protected $fileName = __DIR__.'/../files/sanity/201812031105_check_column_definitions.blm';
+    protected $blm = null;
+    
+    public function listColumnDefinitions()
+    {
+        $result = [];
+
+        $blm = new BlmFile();
+        $columns = $blm->getAllColumnDefinitions();
+        foreach($columns as $columnName => $definition) {
+            $result[$columnName] = [$columnName, $definition];
+        }
+
+        return $result;
+    }
     
     public function test_columnDefinitionsTest()
     {
+        // Log::debug("=== ".\basename(__FILE__)." ===");
+
         $file = fopen($this->fileName, "r");
         $this->assertTrue( is_resource($file) );
 
         $blm = new BlmFile();
         $blm->setup($file);
+
         foreach($blm->readData() as $row) {
             // Log::debug("test=".print_r($row,true));
         }
 
-        Log::debug("===".\basename(__FILE__)."===EXIT===");
+        // Log::debug("=== ".\basename(__FILE__)." ===EXIT===");
+    }
+
+    /**
+     * @dataProvider listColumnDefinitions
+     * @test
+     */
+    public function test_allColumnDefinitionsAreInspected($columnName, $columnDefinition)
+    {
+        // Log::debug("=== Test All Column Definitions Are Inspected - '{$columnName}' ===");
+
+        $fileName = __DIR__.'/../files/column_definitions/check-'.$columnName;
+        if ($columnDefinition['recursive']) {
+            $fileName .= '_01';
+        }
+        $fileName .= '.blm';
+
+        $file = fopen($fileName, "r");
+        $this->assertTrue( is_resource($file) );
+        
+        if ($columnDefinition['mandatory']) {
+            $this->expectExceptionMessage("Mandatory column(s) '{$columnName}' missing");
+        }
+
+        $blm = new BlmFile();
+        $blm->setup($file);
+
+        fclose($file);
+        $this->assertFalse( is_resource($file) );
+
+        // Log::debug("=== EXIT - '{$columnName}' ===");
     }
 
 }
