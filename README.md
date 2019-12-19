9c1cc7703e9be853a5d963d41ad8d81f1b5537c5
John Griffiths <johng@Johns-MacBook-Pro.local>
2019-12-19 09:39:38 +0000

    Add test to check backslash is handled correctly at enf of field

--- README.md
+++ README.md
@@ -6,28 +6,3 @@ No advantage over using file_get_contents() and parsing contents.
 
 Instead using PHP gererator function to split processing to discrete chunks so that only the current row is in memory.
 
-# 2019-12-19 Clean up comments
-- Next stage finish inter-field dependencies
-- Move this to history
-- Create real README
-
-# 2019-12-03 Only test available due to example tests being flawed
-```vendor/bin/phpunit --filter test_columnDefinitionsTest -vvv```
-
-
-# 2019-12-01 @Todo
-Duplicate tests using folder instead of using the zip archive 'tests/files/141212100024_FBM_2014120711.zip'
-*    which should not be altered.
-*    Create test file from StartupTest as initial test.
-
-Drop the memory down to a low level.
-*   To force low memory conditions.
-
-Add processing to cope with rows having fields that contain new-line, carriage-return, line-feed characters.
-
-Split monolith into columns and rules.
-
-Allow processing to be relaxed so that the level of strictness can be chosen.
-
-For each rule in the specification write a test, and implement rules in BlmFile.
-*    Look for anomolies such as MEDIA_IMAGE_TEXT_60 having a size of 20 characters but only allowing the contents to be the 3 character string 'EPC'. 
---
+++ tests/Feature/EscapeBackslashBeforeEndOfFieldTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+// ini_set('memory_limit', 21000000);
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Log;
+use Src\BlmFile\BlmFile;
+
+class EscapeBackslashBeforeEndOfFieldTest extends TestCase
+{
+    protected $columnKeys = '';
+    protected $columnData = '';
+
+    // Required Columns
+    private $requiredColumns = [ 
+        'AGENT_REF' => 'XX99XX_FBM2766',
+        'BRANCH_ID' => 'XX99XX',
+        'STATUS_ID' => '1',
+        'CREATE_DATE' => '2019-12-17 15:49:30',
+        'UPDATE_DATE' => '2019-12-17 15:49:30',
+        'DISPLAY_ADDRESS' => 'SZ ADF TestingEstate Agency. ZG Test (ML), Snowdon Drive, Winterhill, Milton Keynes, MK6 1AJ',
+        'PUBLISHED_FLAG' => '1',
+        'LET_FURN_ID' => '',
+        'LET_RENT_FREQUENCY' => '',
+        'TRANS_TYPE_ID' => '1',
+        'BEDROOMS' => '4',
+        'PRICE' => '250000',
+        'PRICE_QUALIFIER' => '',
+        'PROP_SUB_ID' => '0',
+        'ADDRESS_1' => 'SZ ADF TestingEstate Agency. ZG Test (ML)',
+        'ADDRESS_2' => 'Snowdon Drive, Winterhill',
+        'TOWN' => 'Milton Keynes',
+        'POSTCODE1' => 'MK6',
+        'POSTCODE2' => '1AJ',
+        'FEATURE1' => 'House',
+        'FEATURE2' => 'Garden',
+        'FEATURE3' => 'Lake',
+        'SUMMARY' => 'whatever whatever whatever whatever',
+        'DESCRIPTION' => 'whatever whatever whatever whatever whatever whatever',
+        'NEW_HOME_FLAG' => '0',
+        'MEDIA_IMAGE_00' => 'XX99XX_FBM2766_IMG_01.jpg'
+    ];
+    
+    public function test_EscapeBackslashBeforeEndOfFieldTest()
+    {
+        $requiredColumns = $this->requiredColumns;
+        $requiredColumns['SUMMARY'] = 'WHATEVER WHATEVER WHATEVER WHATEVER WHATEVER WHATEVER\\'.chr(92);
+
+        $columnKeys = implode('^', array_keys($requiredColumns)).'^~';
+        $columnData = implode('^', array_values($requiredColumns)).'^~';
+
+        $blm = new BlmFile();
+        $blm->Version = '3';
+
+        $blm->selectVersionColumnDefinitions('3');
+
+        $blm->validateDefinition($columnKeys);
+
+        $row = $blm->validateData($columnData);
+        $this->assertEquals($row['AGENT_REF'], 'XX99XX_FBM2766');
+        // Log::debug($row);
+    }
+
+}
